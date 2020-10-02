799e4c2d1931831cbbe0ce07932ca88c7bbe2d19
Quinn Zipse <qzipse@outlook.com>
2020-10-02 09:23:53 -0500

    Before Testing

--- src/hw1/ImageConverter.java
+++ src/hw1/ImageConverter.java
@@ -1,16 +1,40 @@
 package hw1;
 
-import java.awt.BufferedImage;
+import java.awt.image.BufferedImage;
 
 public class ImageConverter {
-    
+
     public static BufferedImage toBufferedImage(DigitalImage src) {
-        // TODO : write this method
-        return null;
+        var outImage = new BufferedImage(src.getWidth(), src.getHeight(),
+                (src instanceof IndexedDigitalImage ? BufferedImage.TYPE_BYTE_INDEXED : BufferedImage.TYPE_INT_RGB));
+
+        for (int y = 0; y < src.getHeight(); y++) {
+            for (int x = 0; x < src.getWidth(); x++) {
+                var rgb = src.getPixel(x, y);
+                outImage.setRGB(rgb[0], rgb[1], rgb[2]);
+            }
+        }
+
+        return outImage;
     }
-    
+
     public static DigitalImage toDigitalImage(BufferedImage src) {
-        // TODO : write this method
-        return null;
+
+        DigitalImage outImage;
+
+        if (src.getType() == BufferedImage.TYPE_BYTE_INDEXED) {
+            outImage = new IndexedDigitalImage(src.getWidth(), src.getHeight());
+        } else {
+            outImage = new PackedPixelImage(src.getWidth(), src.getHeight(), 3);
+        }
+
+        for (int y = 0; y < src.getHeight(); y++) {
+            for (int x = 0; x < src.getWidth(); x++) {
+                var rgb = src.getRGB(x, y);
+                outImage.setPixel(x, y, new int[]{rgb >> 16 & 0xFF, rgb >> 8 & 0xFF, rgb & 0xFF});
+            }
+        }
+
+        return outImage;
     }
 }
