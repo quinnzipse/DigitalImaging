bab2c0e618636029b62602f66be9ef0bd015f099
Quinn Zipse <qzipse@outlook.com>
2020-10-12 15:36:31 -0500

    Switched to use NullOp

--- src/hw2/DiagonalFlipOp.java
+++ src/hw2/DiagonalFlipOp.java
@@ -1,16 +1,15 @@
 package hw2;
 
+import pixeljelly.ops.NullOp;
 import pixeljelly.ops.PluggableImageOp;
 
-import java.awt.*;
-import java.awt.geom.Point2D;
-import java.awt.geom.Rectangle2D;
-import java.awt.image.*;
+import java.awt.image.BufferedImage;
+import java.awt.image.BufferedImageOp;
 
-public class DiagonalFlipOp implements PluggableImageOp, BufferedImageOp {
+public class DiagonalFlipOp extends NullOp implements PluggableImageOp, BufferedImageOp {
     @Override
     public BufferedImageOp getDefault(BufferedImage bufferedImage) {
-        return null;
+        return new DiagonalFlipOp();
     }
 
     @Override
@@ -20,26 +19,6 @@ public class DiagonalFlipOp implements PluggableImageOp, BufferedImageOp {
 
     @Override
     public BufferedImage filter(BufferedImage src, BufferedImage dest) {
-        return null;
-    }
-
-    @Override
-    public Rectangle2D getBounds2D(BufferedImage src) {
-        return null;
-    }
-
-    @Override
-    public BufferedImage createCompatibleDestImage(BufferedImage src, ColorModel destCM) {
-        return null;
-    }
-
-    @Override
-    public Point2D getPoint2D(Point2D srcPt, Point2D dstPt) {
-        return null;
-    }
-
-    @Override
-    public RenderingHints getRenderingHints() {
         return null;
     }
 }
--- src/hw2/HorizontalFlipOp.java
+++ src/hw2/HorizontalFlipOp.java
@@ -1,18 +1,15 @@
 package hw2;
 
+import pixeljelly.ops.NullOp;
 import pixeljelly.ops.PluggableImageOp;
 
-import java.awt.*;
-import java.awt.geom.Point2D;
-import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferedImage;
 import java.awt.image.BufferedImageOp;
-import java.awt.image.ColorModel;
 
-public class HorizontalFlipOp implements PluggableImageOp, BufferedImageOp {
+public class HorizontalFlipOp extends NullOp implements PluggableImageOp, BufferedImageOp {
     @Override
     public BufferedImageOp getDefault(BufferedImage bufferedImage) {
-        return null;
+        return new HorizontalFlipOp();
     }
 
     @Override
@@ -22,26 +19,8 @@ public class HorizontalFlipOp implements PluggableImageOp, BufferedImageOp {
 
     @Override
     public BufferedImage filter(BufferedImage src, BufferedImage dest) {
+
         return null;
     }
 
-    @Override
-    public Rectangle2D getBounds2D(BufferedImage src) {
-        return null;
-    }
-
-    @Override
-    public BufferedImage createCompatibleDestImage(BufferedImage src, ColorModel destCM) {
-        return new BufferedImage(src.getWidth(), src.getHeight(), src.getType());
-    }
-
-    @Override
-    public Point2D getPoint2D(Point2D srcPt, Point2D dstPt) {
-        return null;
-    }
-
-    @Override
-    public RenderingHints getRenderingHints() {
-        return null;
-    }
 }
