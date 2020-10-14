0a6951647cf50b16b77f87f9ac2af9d45e63e26b
Quinn Zipse <qzipse@outlook.com>
2020-10-14 15:50:01 -0500

    First attempt at the localEqualizeOp

--- src/hw2/LocalEqualizeOp.java
+++ src/hw2/LocalEqualizeOp.java
@@ -7,12 +7,25 @@ import pixeljelly.scanners.RasterScanner;
 
 import java.awt.image.BufferedImage;
 import java.awt.image.BufferedImageOp;
+import java.awt.image.WritableRaster;
 
 public class LocalEqualizeOp extends NullOp implements PluggableImageOp {
+    private int w, h;
+    private boolean isBanded;
+
+    public LocalEqualizeOp() {
+
+    }
+
+    public LocalEqualizeOp(int w, int h, boolean brightnessBandOnly) {
+        this.w = w;
+        this.h = h;
+        this.isBanded = !brightnessBandOnly;
+    }
 
     @Override
     public BufferedImageOp getDefault(BufferedImage bufferedImage) {
-        return new LocalEqualizeOp();
+        return new LocalEqualizeOp(5, 5, true);
     }
 
     @Override
@@ -26,7 +39,16 @@ public class LocalEqualizeOp extends NullOp implements PluggableImageOp {
             dest = createCompatibleDestImage(src, src.getColorModel());
         }
 
-        for (Location pt : new RasterScanner(dest, true)) {
+        for (Location pt : new RasterScanner(dest, isBanded)) {
+            int sum = 0;
+            WritableRaster r = src.getRaster();
+            for (int y = -h / 2; y < h / 2; y++) {
+                for (int x = -w / 2; x < w / 2; x++) {
+                    sum += r.getSample(pt.col + x, pt.row + y, pt.band);
+                }
+            }
+
+            dest.getRaster().setSample(pt.col, pt.row, pt.band, sum);
         }
 
         return dest;
