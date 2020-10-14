fe10b55bd72c70dba8f2c68631b470952bc46b22
Quinn Zipse <qzipse@outlook.com>
2020-10-14 13:48:13 -0500

    Diagonal Done

--- src/hw2/DiagonalFlipOp.java
+++ src/hw2/DiagonalFlipOp.java
@@ -2,6 +2,8 @@ package hw2;
 
 import pixeljelly.ops.NullOp;
 import pixeljelly.ops.PluggableImageOp;
+import pixeljelly.scanners.Location;
+import pixeljelly.scanners.RasterScanner;
 
 import java.awt.image.BufferedImage;
 import java.awt.image.BufferedImageOp;
@@ -19,6 +21,15 @@ public class DiagonalFlipOp extends NullOp implements PluggableImageOp {
 
     @Override
     public BufferedImage filter(BufferedImage src, BufferedImage dest) {
-        return null;
+        if(dest == null) {
+            dest = createCompatibleDestImage(src, src.getColorModel());
+        }
+
+        for(Location pt : new RasterScanner(src, true)){
+            int sample = src.getRaster().getSample(pt.col, pt.row, pt.band);
+            dest.getRaster().setSample(Math.abs((src.getWidth() - 1) - pt.col), Math.abs(src.getHeight() - 1 - pt.col), pt.band, sample);
+        }
+
+        return dest;
     }
 }
