9c1cd0ba33fe8404297eaafe0d1da3a04a4610fb
Quinn Zipse <qzipse@outlook.com>
2020-11-01 01:12:42 -0500

    Updating

--- src/hw3/FastMedianOp.java
+++ src/hw3/FastMedianOp.java
@@ -1,2 +1,61 @@
-package hw3;public class FastMedianOp {
+package hw3;
+
+import pixeljelly.ops.NullOp;
+import pixeljelly.ops.PluggableImageOp;
+import pixeljelly.scanners.Location;
+import pixeljelly.scanners.RasterScanner;
+
+import java.awt.image.BufferedImage;
+import java.awt.image.BufferedImageOp;
+
+public class FastMedianOp extends NullOp implements PluggableImageOp {
+
+    private int n;
+    private int m;
+
+    public FastMedianOp() {
+
+    }
+
+    public FastMedianOp(int n, int m) {
+        this.m = m;
+        this.n = n;
+    }
+
+    @Override
+    public BufferedImageOp getDefault(BufferedImage bufferedImage) {
+        return new FastMedianOp(9, 9);
+    }
+
+    @Override
+    public String getAuthorName() {
+        return "Quinn Zipse";
+    }
+
+    @Override
+    public BufferedImage filter(BufferedImage src, BufferedImage dest) {
+        if (dest == null) {
+            dest = createCompatibleDestImage(src, src.getColorModel());
+        }
+
+        float[] histogram = new float[m * n];
+        int c = 0;
+
+        // for every row R of the src (top-to-bottom)
+        for (int r = 0; r < src.getHeight(); r++) {
+
+            // set H to be the histogram of the NxN region of first sample
+            // find the median value MED by examining H
+            // write MED to DEST
+            dest.getRaster().setSample(r, 0, );
+            // for every remaining column C of R (left-to-right)
+            for (; c < src.getWidth(); c++) {
+                // update H by removing the value in the left most column of the previous region
+                // update H by adding the values in the right most column of the current region.
+                // find the median value MED by scanning higher or lower depending on the values that have been removed/added
+                // write MED to DEST
+            }
+        }
+        return super.filter(src, dest);
+    }
 }
--- src/hw3/GaussianOp.java
+++ src/hw3/GaussianOp.java
@@ -1,2 +1,74 @@
-package hw3;public class GaussianOp {
+package hw3;
+
+import pixeljelly.ops.ConvolutionOp;
+import pixeljelly.ops.NullOp;
+import pixeljelly.ops.PluggableImageOp;
+import pixeljelly.utilities.SeperableKernel;
+
+import java.awt.image.BufferedImage;
+import java.awt.image.BufferedImageOp;
+
+public class GaussianOp extends NullOp implements PluggableImageOp {
+
+    private double alpha;
+    private double sigma;
+
+    public double getAlpha() {
+        return alpha;
+    }
+
+    public void setAlpha(double alpha) {
+        this.alpha = alpha;
+    }
+
+    public double getSigma() {
+        return sigma;
+    }
+
+    public void setSigma(double sigma) {
+        this.sigma = sigma;
+    }
+
+    public GaussianOp() {
+        alpha = 1;
+        sigma = 1;
+    }
+
+    public GaussianOp(double alpha, double sigma) {
+        this.alpha = alpha;
+        this.sigma = sigma;
+    }
+
+    @Override
+    public BufferedImageOp getDefault(BufferedImage bufferedImage) {
+        return new GaussianOp(2, 3);
+    }
+
+    @Override
+    public String getAuthorName() {
+        return "Quinn Zipse";
+    }
+
+    @Override
+    public BufferedImage filter(BufferedImage src, BufferedImage dest) {
+        if (dest == null) {
+            dest = createCompatibleDestImage(src, src.getColorModel());
+        }
+
+        float[] coefficients = getGaussianCoefficients(alpha, sigma);
+        dest = (new ConvolutionOp(new SeperableKernel(coefficients, coefficients), false)).filter(src, dest);
+
+        return dest;
+    }
+
+    private float[] getGaussianCoefficients(double alpha, double sigma) {
+        int w = (int) Math.ceil(alpha * sigma);
+        float[] result = new float[w * 2 + 1];
+        for (int n = 0; n <= w; n++) {
+            double coefficient = Math.exp(-(n * n) / (2 * sigma * sigma)) / (Math.sqrt(2 * Math.PI) * sigma);
+            result[w + n] = (float) coefficient;
+            result[w - n] = (float) coefficient;
+        }
+        return result;
+    }
 }
