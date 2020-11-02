594ccff0ae309ffd807cb5258d1e9cd616bd03bd
Quinn Zipse <qzipse@outlook.com>
2020-11-02 14:08:09 -0600

    That didn't really work so I'm going to rethink it

--- src/hw3/FastMedianOp.java
+++ src/hw3/FastMedianOp.java
@@ -1,16 +1,12 @@
 package hw3;
 
-import pixeljelly.features.Histogram;
 import pixeljelly.ops.NullOp;
 import pixeljelly.ops.PluggableImageOp;
-import pixeljelly.scanners.Location;
-import pixeljelly.scanners.RasterScanner;
+import pixeljelly.utilities.ImagePadder;
 import pixeljelly.utilities.ReflectivePadder;
 
 import java.awt.image.BufferedImage;
 import java.awt.image.BufferedImageOp;
-import java.util.ArrayDeque;
-import java.util.Arrays;
 
 public class FastMedianOp extends NullOp implements PluggableImageOp {
 
@@ -43,32 +39,31 @@ public class FastMedianOp extends NullOp implements PluggableImageOp {
         }
 
         // Assumes RGB 24bpp Image.
-        var rH = new int[256];
-        var gH = new int[256];
-        var bH = new int[256];
+        int[][] histograms = new int[3][256];
 
         // for every row R of the src (top-to-bottom)
         for (int r = 0; r < src.getHeight(); r++) {
 
             // set H to be the histogram of the NxN region of first sample
-            var padder = ReflectivePadder.getInstance();
+            ImagePadder padder = ReflectivePadder.getInstance();
             for (int y = -n / 2; y < n / 2; y++) {
                 for (int x = -m / 2; x < m / 2; x++) {
-                    rH[padder.getSample(src, y, x, 0)]++;
-                    gH[padder.getSample(src, y, x, 1)]++;
-                    bH[padder.getSample(src, y, x, 2)]++;
+                    for (int b = 0; b < 3; b++) {
+                        histograms[b][padder.getSample(src, y, x, b)]++;
+                    }
                 }
             }
 
+
             // find the median value MED by examining H
-            var rM = getMedian(rH);
-            var gM = getMedian(gH);
-            var bM = getMedian(bH);
+            int[] rMED_CDF = getMedian(rH);
+            int[] gMED_CDF = getMedian(gH);
+            int[] bMED_CDF = getMedian(bH);
 
             // write MED to DEST
-            dest.getRaster().setSample(r, 0, 0, rM);
-            dest.getRaster().setSample(r, 0, 1, gM);
-            dest.getRaster().setSample(r, 0, 2, bM);
+            dest.getRaster().setSample(0, r, 0, rMED_CDF[0]);
+            dest.getRaster().setSample(0, r, 1, gMED_CDF[0]);
+            dest.getRaster().setSample(0, r, 2, bMED_CDF[0]);
 
             // for every remaining column C of R (left-to-right)
             for (int c = 0; c < src.getWidth(); c++) {
@@ -76,38 +71,77 @@ public class FastMedianOp extends NullOp implements PluggableImageOp {
                 for (int remove = -n / 2; remove < n / 2; remove++) {
                     int rN = padder.getSample(src, r + remove, c, 0);
                     rH[rN]--;
-                    if(rN < rM) rM++;
-                    gH[padder.getSample(src, r + remove, c, 1)]--;
-                    bH[padder.getSample(src, r + remove, c, 2)]--;
+                    if (rN < rMED_CDF[0]) rMED_CDF[1]--;
+
+                    int gN = padder.getSample(src, r + remove, c, 1);
+                    gH[gN]--;
+                    if (gN < gMED_CDF[0]) gMED_CDF[1]--;
+
+                    int bN = padder.getSample(src, r + remove, c, 2);
+                    bH[bN]--;
+                    if (bN < bMED_CDF[0]) bMED_CDF[1]--;
                 }
 
                 // update H by adding the values in the right most column of the current region.
                 for (int remove = -n / 2; remove < n / 2; remove++) {
-                    rH[padder.getSample(src, r + remove, c + m, 0)]--;
-                    gH[padder.getSample(src, r + remove, c + m, 1)]--;
-                    bH[padder.getSample(src, r + remove, c + m, 2)]--;
+                    int rN = padder.getSample(src, r + remove, c, 0);
+                    rH[rN]++;
+                    if (rN > rMED_CDF[0]) rMED_CDF[1]++;
+
+                    int gN = padder.getSample(src, r + remove, c, 1);
+                    gH[gN]++;
+                    if (gN > gMED_CDF[0]) gMED_CDF[1]++;
+
+                    int bN = padder.getSample(src, r + remove, c, 2);
+                    bH[bN]++;
+                    if (bN < bMED_CDF[0]) bMED_CDF[1]++;
                 }
 
                 // find the median value MED by scanning higher or lower depending on the values that have been removed/added
+                while (rMED_CDF[1] < Math.ceil(m * n / 2.0)) {
+                    rMED_CDF[1] += rH[rMED_CDF[0]];
+                    rMED_CDF[0]++;
+                }
+                while (rMED_CDF[1] - rH[rMED_CDF[0]] >= Math.ceil(m * n / 2.0)) {
+                    rMED_CDF[1] -= rH[rMED_CDF[0]];
+                    rMED_CDF[0]--;
+                }
+                while (gMED_CDF[1] < Math.ceil(m * n / 2.0)) {
+                    gMED_CDF[1] += gH[gMED_CDF[0]];
+                    gMED_CDF[0]++;
+                }
+                while (gMED_CDF[1] - gH[gMED_CDF[0]] >= Math.ceil(m * n / 2.0)) {
+                    gMED_CDF[1] -= gH[gMED_CDF[0]];
+                    gMED_CDF[0]--;
+                }
+                while (bMED_CDF[1] < Math.ceil(m * n / 2.0)) {
+                    bMED_CDF[1] += bH[bMED_CDF[0]];
+                    bMED_CDF[0]++;
+                }
+                while (bMED_CDF[1] - bH[gMED_CDF[0]] >= Math.ceil(m * n / 2.0)) {
+                    bMED_CDF[1] -= bH[gMED_CDF[0]];
+                    bMED_CDF[0]--;
+                }
+
                 // write MED to DEST
-                dest.getRaster().setSample(r, 0, 0, rM);
-                dest.getRaster().setSample(r, 0, 1, gM);
-                dest.getRaster().setSample(r, 0, 2, bM);
+                dest.getRaster().setSample(c, r, 0, rMED_CDF[0]);
+                dest.getRaster().setSample(c, r, 1, gMED_CDF[0]);
+                dest.getRaster().setSample(c, r, 2, bMED_CDF[0]);
             }
         }
         return super.filter(src, dest);
     }
 
-    private int getMedian(int[] histogram) {
-        var totalSamples = m * n;
-        var countedSamples = 0;
-        var i = 0;
+    private int[] getMedian(int[] histogram) {
+        int totalSamples = m * n;
+        int countedSamples = 0;
+        int i = 0;
 
         while (countedSamples < totalSamples / 2) {
             countedSamples += histogram[i];
             i++;
         }
 
-        return i;
+        return new int[]{i, countedSamples};
     }
 }
