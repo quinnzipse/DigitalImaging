536557c4833a1c4b5c0b244703a4de567e01fdcc
Quinn Zipse <qzipse@outlook.com>
2020-11-08 09:37:13 -0600

    Calculating colors

--- src/hw3/SimilarityMatrix.java
+++ src/hw3/SimilarityMatrix.java
@@ -22,8 +22,20 @@ public class SimilarityMatrix {
         float[][] a = new float[rn * gn * bn][rn * gn * bn];
         for (int i = 0; i < a.length; i++) {
             for (int j = 0; j < a.length; j++) {
-                Color c1 = new Color(i / (gn * bn), i / bn % gn, i % bn);
-                Color c2 = new Color(j / (gn * bn), j / bn % gn, j % bn);
+                // get the two colors to compare based on the matrix.
+                int ir = i / (gn * bn);
+                int ig = i / bn % gn;
+                int ib = i % bn;
+                Color c1 = new Color(ir * (256 / rn) + 128 / rn, ig * (256 / gn) + 128 / gn, ib * (256 / bn) + 128 / bn);
+                Color c2 = new Color(jr * (256 / rn) + 128 / rn, jg * (256 / gn) + 128 / gn, jb * (256 / bn) + 128 / bn);
+
+                // Do we need to find the center of the bin?
+
+                // Calculate the L2 Distance for those two colors!
+                int distance = 0;
+
+                // Store that number in the similarity matrix! Should be 0-1
+                a[i][j] = distance;
             }
         }
         return a;
