09d298be5a2e1c66761589b6aa8ca8e2da768aa3
Quinn Zipse <qzipse@outlook.com>
2020-11-13 21:01:32 -0600

    Working on the median cut algorithm

--- src/hw3/GaussianOp.java
+++ src/hw3/GaussianOp.java
@@ -7,6 +7,7 @@ import pixeljelly.utilities.SeperableKernel;
 
 import java.awt.image.BufferedImage;
 import java.awt.image.BufferedImageOp;
+import java.util.stream.Stream;
 
 public class GaussianOp extends NullOp implements PluggableImageOp {
 
--- src/hw4/DitherOp.java
+++ src/hw4/DitherOp.java
@@ -1,12 +1,17 @@
 package hw4;
 
 import pixeljelly.features.Histogram;
+import pixeljelly.gui.InterruptableTask;
 import pixeljelly.ops.NullOp;
 import pixeljelly.ops.PluggableImageOp;
+import pixeljelly.scanners.Location;
+import pixeljelly.scanners.RasterScanner;
 
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.awt.image.BufferedImageOp;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class DitherOp extends NullOp implements PluggableImageOp {
 
@@ -28,17 +33,65 @@ public class DitherOp extends NullOp implements PluggableImageOp {
         this.palette = palette;
     }
 
-    private Color[] generateOptimalPalette(int size, BufferedImage src) {
+    private Color[] medianCut(int size, BufferedImage src) {
+        int[][] samples = new int[src.getRaster().getNumBands()][src.getSampleModel().getSampleSize(0)];
+        int[] ranges = new int[src.getRaster().getNumBands()];
 
-        Histogram red = new Histogram(src, 0);
-        Histogram green = new Histogram(src, 1);
-        Histogram blue = new Histogram(src, 2);
+        for (Location pt : new RasterScanner(src, true)) {
+            samples[pt.band][src.getRaster().getSample(pt.col, pt.row, pt.band)]++;
+        }
 
-        int redMedian = 0;
-        int greenMedian = 0;
-        int blueMedian = 0;
+        for (int i = 0; i < samples.length; i++) {
+            int[] band = samples[i];
+            int min = 0;
+            int max = band.length - 1;
 
-        return new Color[0];
+            while (band[min] == 0) {
+                min++;
+            }
+
+            while (band[max] == 0) {
+                max--;
+            }
+
+            ranges[i] = max - min;
+        }
+
+        int maxRangeIndex = 0;
+        for (int i = 1; i < ranges.length; i++) {
+            if (maxRangeIndex < ranges[i]) {
+                maxRangeIndex = i;
+            }
+        }
+
+        // We know what band has the most range...
+
+
+
+    }
+
+    private Color[] medianCut1(int size, BufferedImage src) {
+        int[] max = new int[src.getRaster().getNumBands()];
+        int[] min = new int[src.getRaster().getNumBands()];
+        int[] range = new int[src.getRaster().getNumBands()];
+
+        for (Location pt : new RasterScanner(src, true)) {
+            int sample = src.getRaster().getSample(pt.col, pt.row, pt.band);
+            max[pt.band] = Math.max(max[pt.band], sample);
+            min[pt.band] = Math.min(min[pt.band], sample);
+        }
+
+        int maxOfMax = 0;
+        int index = -1;
+        for (int i = 0; i < max.length; i++) {
+            range[i] = max[i] - min[i];
+            if (range[i] > maxOfMax) {
+                maxOfMax = range[i];
+                index = i;
+            }
+        }
+
+        double median = maxOfMax / 2.0;
     }
 
     @Override
