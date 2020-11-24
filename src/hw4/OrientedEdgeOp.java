3a6cf8eeb3f5cd333e38cbfb1005dad6061d9c68
Quinn Zipse <qzipse@outlook.com>
2020-11-24 15:07:38 -0600

    OrientedEdge needs fine tuning

--- src/hw4/OrientedEdgeOp.java
+++ src/hw4/OrientedEdgeOp.java
@@ -1,9 +1,15 @@
 package hw4;
 
-import pixeljelly.ops.*;
+import pixeljelly.ops.BandExtractOp;
+import pixeljelly.ops.ConvolutionOp;
+import pixeljelly.ops.NullOp;
+import pixeljelly.ops.PluggableImageOp;
+import pixeljelly.scanners.Location;
+import pixeljelly.scanners.RasterScanner;
 import pixeljelly.utilities.NonSeperableKernel;
 import pixeljelly.utilities.SimpleColorModel;
 
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.awt.image.BufferedImageOp;
 
@@ -11,15 +17,27 @@ public class OrientedEdgeOp extends NullOp implements PluggableImageOp {
     private double strength;
     private double orientation;
     private double epsilon;
+    private boolean invertMax, invertMin;
+    private double max, min;
 
     public OrientedEdgeOp() {
-
+        this(.5, .75, .15);
     }
 
     public OrientedEdgeOp(double strength, double orientation, double epsilon) {
         this.epsilon = epsilon;
         this.orientation = orientation;
         this.strength = strength;
+        max = this.orientation + epsilon;
+        if (max > 1) {
+            max -= 1;
+            invertMax = true;
+        }
+        min = this.orientation - epsilon;
+        if (min < 0) {
+            min += 1;
+            invertMin = true;
+        }
     }
 
     @Override
@@ -70,9 +88,30 @@ public class OrientedEdgeOp extends NullOp implements PluggableImageOp {
         BufferedImage xConvolve = (new ConvolutionOp(gX, false)).filter(temp, null);
         BufferedImage yConvolve = (new ConvolutionOp(gY, false)).filter(temp, null);
 
-        BufferedImageOp mog = new AddBinaryOp(xConvolve);
+        double maxDist = 0;
 
-        mog.filter(yConvolve, dest);
+        for (Location pt : new RasterScanner(temp, false)) {
+            // The convolved images are greyscale (0-255) based on their x and y differences respectively.
+            int dx = xConvolve.getRaster().getSample(pt.col, pt.row, 0);
+            int dy = yConvolve.getRaster().getSample(pt.col, pt.row, 0);
+
+            // Convert to polar coordinates
+            double distance = Math.sqrt(dx * dx + dy * dy) / 255;
+            double direction = Math.atan2(dy, dx);
+
+            if (distance > strength) {
+                maxDist = Math.max(maxDist, distance);
+                dest.setRGB(pt.col, pt.row, Color.GREEN.getRGB());
+
+                if (direction < max && min < direction) {
+                    dest.setRGB(pt.col, pt.row, Color.RED.getRGB());
+                }
+            } else {
+                dest.setRGB(pt.col, pt.row, src.getRGB(pt.col, pt.row));
+            }
+        }
+
+        System.out.println("Max Dist: " + maxDist);
 
         return dest;
     }
