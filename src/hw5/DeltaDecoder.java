30bb7fd021226f051f16dcb5a865d274dc017e4c
Quinn Zipse <qzipse@outlook.com>
2020-12-13 14:56:00 -0600

    Making changes

--- src/hw5/DeltaDecoder.java
+++ src/hw5/DeltaDecoder.java
@@ -36,25 +36,31 @@ public class DeltaDecoder extends ImageDecoder {
         deltas = new int[destImg.getRaster().getNumBands()];
 
         for (int i = 0; i < deltas.length; i++) {
-            deltas[i] = inStream.readByte();
+            deltas[i] = inStream.read();
             System.out.print(deltas[i] + " ");
         }
+
         System.out.println();
 
         int prev = 0;
 
         for (int b = 0; b < destImg.getRaster().getNumBands(); b++) {
             System.out.println(b);
+            int i=0;
             for (Location pt : new RasterScanner(destImg, false)) {
-
                 if (pt.col == 0) {
-                    prev = inStream.readByte();
+                    while((i % 7) != 0){
+                        i++;
+                        inStream.readBit();
+                    }
+                    prev = inStream.read();
+                    i=0;
                 } else if (inStream.readBit() == 0) {
-//                    prev -= deltas[b];
+                    prev -= deltas[b];
                 } else {
-//                    prev += deltas[b];
+                    prev += deltas[b];
                 }
-
+                i++;
                 destImg.getRaster().setSample(pt.col, pt.row, b, prev);
             }
         }
